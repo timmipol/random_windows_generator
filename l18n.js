2f27ce35948b4f502381fd1fa8a69830bfb75ca2
timmipol <sinelnikov.timofei@gmail.com>
2021-05-20 17:56:59 +0300

    added translation script

---
+++ l18n.js
@@ -0,0 +1,29 @@
+en = i18n.create({
+  values: {
+    "title":"Windows Random Generator (Beta)",
+    "header": "Windows Random Generator (Beta)",
+    "error": "Oops, error occured, try again, please." ,
+    "button": "Next Random Windows",
+    "footer": "© 2021 Made By Timmipol"
+}
+})
+  
+ru = i18n.create({
+  values: {
+    "title":"Генератор рандомных Windows (Beta)",
+    "header": "Генератор рандомных Windows (Beta)",
+    "error": "Ой, случилась ошибка, попробуйте снова." ,
+    "button": "Следующий рандомный Windows",
+    "footer": "© 2021 Сделал Timmipol"
+  }
+})
+
+ua = i18n.create({
+  values: {
+    "title":"Генератор рандомних Windows (Beta)",
+    "header": "Генератор рандомних Windows (Beta)",
+    "error": "Ой, трапилася помилка, спробуйте ще раз." ,
+    "button": "Наступний рандомний Windows",
+    "footer": "© 2021 Зробив Timmipol"
+  }
+})
